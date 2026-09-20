82b19ab4f8810fe59431c5ae8323baae9c82337a
toimantoim <soutofernando047@gmail.com>
2026-09-20 16:47:15 -0300

    Add files via upload

---
+++ barcafurada.py
@@ -0,0 +1,11 @@
+captains={}
+captains['Enterprise']='Picard'
+captains['Voyager']='Janeway'
+captains['Defiant']='Sisko'
+captains['Barca brasileira furada']='Dunga'
+captains['Do olho do cu']='Bolsonaro'
+print(captains)
+if "plum_plac_zum" not in captains:
+    captains["plum_plac_zum"]='Unknown'
+for barca,capitão in captains.items():
+          print(barca,capitão)
