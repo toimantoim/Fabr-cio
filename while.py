cbae20cef8b27009c7ccf9dcf750caed69b59a73
toimantoim <soutofernando047@gmail.com>
2026-09-20 16:50:30 -0300

    Add files via upload

---
+++ while.py
@@ -0,0 +1,6 @@
+Python 3.14.7 (tags/v3.14.7:823f032, Aug  5 2026, 10:51:32) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> n=1
+>>> while n<5:
+...     print(n)
+...     n=n+1
